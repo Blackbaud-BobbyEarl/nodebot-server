1406137ea1c1537a1cc96bc4c131447b35fe7feb
Bobby Earl <bobby.earl@blackbaud.com>
2015-07-26 11:16:24 -0400

    Changed to nodebot

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
 server.listen(port)
+console.log('http server listening on %d', port);
 
-console.log("http server listening on %d", port)
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+wss.on('connection', function(ws) {
+    console.log('websocket connection opened');
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    ws.on('message', function (data) {
+        console.log('received: ' + data);
+        wss.broadcast(data);
+    });
 
-  console.log("websocket connection open")
+    ws.on('close', function() {
+        console.log('websocket connection closed');
+    });
+});
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
